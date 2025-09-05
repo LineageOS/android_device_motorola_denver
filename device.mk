e6b248f61677739b7fff5f7df22b455b79174f0f
Michael Bestas <mkbestas@lineageos.org>
2025-09-05 11:55:25 +0300

    denver: Enable LiveDisplay adaptive backlight
    Change-Id: I3892e178b87b569805dd5e929e009d465bd63461

--- device.mk
+++ device.mk
@@ -56,6 +56,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# LiveDisplay
+$(call soong_config_set,livedisplay_sysfs,enable_ab,true)
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
